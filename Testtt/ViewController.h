a106b32e5fd86a34abf01be8b1d6cd5c968280b4
Katsav Eran <erankatsav@gmail.com>
2018-06-11 11:31:47 +0300

    Initial Commit

---
+++ Testtt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Testtt
+//
+//  Created by Eran Katsav on 11/06/2018.
+//  Copyright © 2018 Eran Katsav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Testtt/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Testtt
+//
+//  Created by Eran Katsav on 11/06/2018.
+//  Copyright © 2018 Eran Katsav. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
